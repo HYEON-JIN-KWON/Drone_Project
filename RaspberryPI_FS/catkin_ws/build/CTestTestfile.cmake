5c6d7d8e13eb54269e8777ed4a7440b3cd2b67db
HYEON-JIN-KWON <55308496+HYEON-JIN-KWON@users.noreply.github.com>
2021-06-03 11:12:30 +0900

    Add files via upload

---
+++ RaspberryPI_FS/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/edgeilab/catkin_ws/src
+# Build directory: /home/edgeilab/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("realsense-ros/realsense2_description")
+subdirs("Onboard-SDK-ROS/dji_sdk")
+subdirs("Onboard-SDK-ROS/dji_sdk_demo")
+subdirs("realsense-ros/realsense2_camera")
