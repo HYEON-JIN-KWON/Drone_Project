2eed5fba47863e4de7484c06be68f5a312501854
HYEON-JIN-KWON <55308496+HYEON-JIN-KWON@users.noreply.github.com>
2021-06-03 13:32:38 +0900

    Add files via upload

---
+++ GPU_Computer/catkin_ws/build/dji_pkg/CMakeFiles/dji_pkg_generate_messages_nodejs.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dji_pkg_generate_messages_nodejs"
+  "/home/pnu/catkin_ws/devel/share/gennodejs/ros/dji_pkg/msg/Pos_XYZ_th.js"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/dji_pkg_generate_messages_nodejs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ GPU_Computer/catkin_ws/build/dji_pkg/CMakeFiles/dji_pkg_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dji_pkg_generate_messages_py"
+  "/home/pnu/catkin_ws/devel/lib/python2.7/dist-packages/dji_pkg/msg/_Pos_XYZ_th.py"
+  "/home/pnu/catkin_ws/devel/lib/python2.7/dist-packages/dji_pkg/msg/__init__.py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/dji_pkg_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
