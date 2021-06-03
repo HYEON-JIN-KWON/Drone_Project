6a4639515e46eaceebc1b311a59fcfcd9d03ed47
HYEON-JIN-KWON <55308496+HYEON-JIN-KWON@users.noreply.github.com>
2021-06-03 11:48:41 +0900

    Add files via upload

---
+++ RaspberryPI_FS/Onboard-SDK/build/sample/linux/payload-3rd-party/CMakeFiles/djiosdk-payload-control-sample.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/Onboard-SDK/sample/linux/common/dji_linux_environment.cpp" "/home/ubuntu/Onboard-SDK/build/sample/linux/payload-3rd-party/CMakeFiles/djiosdk-payload-control-sample.dir/__/common/dji_linux_environment.cpp.o"
+  "/home/ubuntu/Onboard-SDK/sample/linux/common/dji_linux_helpers.cpp" "/home/ubuntu/Onboard-SDK/build/sample/linux/payload-3rd-party/CMakeFiles/djiosdk-payload-control-sample.dir/__/common/dji_linux_helpers.cpp.o"
+  "/home/ubuntu/Onboard-SDK/sample/linux/payload-3rd-party/main.cpp" "/home/ubuntu/Onboard-SDK/build/sample/linux/payload-3rd-party/CMakeFiles/djiosdk-payload-control-sample.dir/main.cpp.o"
+  "/home/ubuntu/Onboard-SDK/sample/linux/payload-3rd-party/payload-control-sample.cpp" "/home/ubuntu/Onboard-SDK/build/sample/linux/payload-3rd-party/CMakeFiles/djiosdk-payload-control-sample.dir/payload-control-sample.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../sample/linux/../../osdk-core/api/inc"
+  "../sample/linux/../../osdk-core/utility/inc"
+  "../sample/linux/../../osdk-core/hal/inc"
+  "../sample/linux/../../osdk-core/protocol/inc"
+  "../sample/linux/../../osdk-core/platform/linux/inc"
+  "../sample/linux/payload-3rd-party/../common"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ubuntu/Onboard-SDK/build/osdk-core/CMakeFiles/djiosdk-core.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
