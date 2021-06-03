6eb43cc547402640485faf3b390fa3e3e3ed61b8
HYEON-JIN-KWON <55308496+HYEON-JIN-KWON@users.noreply.github.com>
2021-06-03 13:25:12 +0900

    Add files via upload

---
+++ GPU_Computer/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/pnu/catkin_ws/src
+# Build directory: /home/pnu/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("dji_pkg")
+subdirs("darknet_ros/darknet_ros_msgs")
+subdirs("darknet_ros/darknet_ros")
