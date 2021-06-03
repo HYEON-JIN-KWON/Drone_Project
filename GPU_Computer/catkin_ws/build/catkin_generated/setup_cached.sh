6e41baa9ec3cb02eb03f05f28e7fdef0a53b2adc
HYEON-JIN-KWON <55308496+HYEON-JIN-KWON@users.noreply.github.com>
2021-06-03 13:28:24 +0900

    Add files via upload

---
+++ GPU_Computer/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH='/home/pnu/catkin_ws/devel/lib:/opt/ros/melodic/lib:~catkin_ws/devel/lib/parrot_arsdk:/home/pnu/catkin_ws/devel/lib/parrot_arsdk:/usr/local/cuda-11.3/extras/CUPTI/lib64:/usr/local/cuda-11.3/lib64'
+export PKG_CONFIG_PATH='/opt/ros/melodic/lib/pkgconfig'
+export PWD='/home/pnu/catkin_ws/build'
